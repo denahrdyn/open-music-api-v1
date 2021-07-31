b0bd7ba6318bd33ed3c7a746c505e13f72091455
Dena Herdiyani <denaherdiyani@gmail.com>
2021-08-01 06:24:27 +0700

    initial commit

---
+++ src/api/songs/handler.js
@@ -0,0 +1,64 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class SongsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postSongHandler = this.postSongHandler.bind(this);
+    this.getSongHandler = this.getSongHandler.bind(this);
+    this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+    this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
+    this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
+  }
+
+  async postSongHandler(request, h) {
+    try {
+      this._validator.validatorSongPayload(request.payload);
+      const {
+        title,
+        year,
+        performer,
+        genre,
+        duration,
+      } = request.payload;
+
+      const songId = await this._service.addSong({
+        title,
+        year,
+        performer,
+        genre,
+        duration,
+      });
+
+      const response = h.response({
+        status: 'success',
+        message: 'Lagu berhasil ditambahkan',
+        data: {
+          songId,
+        },
+      });
+
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      return response;
+    }
+  }
+}
+
+module.exports = SongsHandler;
---
+++ src/api/songs/routes.js
@@ -0,0 +1,12 @@
+const routes = (handler) = [
+  {
+    method: 'POST',
+    path: '/songs',
+    handler: handler.postSonghandler,
+  },
+  {
+    method
+  }
+];
+
+module.exports = routes;
